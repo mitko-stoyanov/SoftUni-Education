009a498cd9344a3c7acde2eda1d2c3abb361f94a
Mitko Stoyanov <mitko_st6@abv.bg>
2021-09-30 21:08:43 +0300

    Homework.

---
+++ Python-Advanced/Functions Advanced/recursion_palindrome.py
@@ -0,0 +1,6 @@
+def palindrome(word, index):
+    if index == len(word) // 2:
+        return f'{word} is a palindrome'
+    if word[index] != word[len(word) - 1 - index]:
+        return f'{word} is not a palindrome'
+    return palindrome(word, index + 1)
