d6e1952c03ae8bc558bf94bf8bb9704905d6c461
Mitko Stoyanov <mitko_st6@abv.bg>
2021-11-08 23:36:04 +0200

    Classwork.

---
+++ Python-OOP/First Steps in OOP/programmer.py
@@ -0,0 +1,21 @@
+class Programmer:
+    def __init__(self, name, language, skills):
+        self.name = name
+        self.language = language
+        self.skills = skills
+
+    def watch_course(self, course_name, language, skills_earned):
+        if language == self.language:
+            self.skills += skills_earned
+            return f"{self.name} watched {course_name}"
+        return f"{self.name} does not know {language}"
+
+    def change_language(self, new_language, skills_needed):
+        if not new_language == self.language and self.skills >= skills_needed:
+            old_language = self.language
+            self.language = new_language
+            return f"{self.name} switched from {old_language} to {new_language}"
+        elif new_language == self.language and self.skills >= skills_needed:
+            return f"{self.name} already knows {new_language}"
+        else:
+            return f"{self.name} needs {skills_needed - self.skills} more skills"
