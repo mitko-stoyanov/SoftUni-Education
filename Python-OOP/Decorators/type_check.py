5a02e2a2decb31778332ccb19988b2ed25cc48b5
Mitko Stoyanov <mitko_st6@abv.bg>
2021-11-26 23:34:25 +0200

    Classwork.

---
+++ Python-OOP/Decorators/type_check.py
@@ -0,0 +1,19 @@
+def type_check(type):
+    def decorator(func):
+        def wrapper(arg):
+            if isinstance(arg, type):
+                return func(arg)
+            return 'Bad Type'
+
+        return wrapper
+
+    return decorator
+
+
+@type_check(int)
+def times2(num):
+    return num * 2
+
+
+print(times2(2))
+print(times2('Not A Number'))
