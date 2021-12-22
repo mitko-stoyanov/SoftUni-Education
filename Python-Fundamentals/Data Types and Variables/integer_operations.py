c671e575566a84e516224116e1d31fccffae4c1a
Mitko Stoyanov <mitko_st6@abv.bg>
2021-12-22 23:27:26 +0200

    Classwork.

---
+++ Python-Fundamentals/Data Types and Variables/integer_operations.py
@@ -0,0 +1,6 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+four_number = int(input())
+result = (first_number + second_number) // third_number * four_number
+print(result)
