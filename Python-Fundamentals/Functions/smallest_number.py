4b6f7809f49a6e4f0c8929723f137b0a05ca6f92
Mitko Stoyanov <mitko_st6@abv.bg>
2022-01-06 23:59:11 +0200

    Classwork.

---
+++ Python-Fundamentals/Functions/smallest_number.py
@@ -0,0 +1,8 @@
+def smallest_num(num_1, num_2, num_3):
+    return min(num_1,num_2,num_3)
+
+number_1 = int(input())
+number_2 = int(input())
+number_3 = int(input())
+
+print(smallest_num(number_1, number_2, number_3))
