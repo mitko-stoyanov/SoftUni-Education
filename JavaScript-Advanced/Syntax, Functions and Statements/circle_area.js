11902d071abb7e4244005392fc89fcb0d3a8712a
Mitko Stoyanov <mitko_st6@abv.bg>
2022-05-22 19:42:25 +0300

    Classwork.

---
+++ JavaScript-Advanced/Syntax, Functions and Statements/circle_area.js
@@ -0,0 +1,10 @@
+function findArea(radius) {
+    if(typeof(radius) == 'number') {
+        let area = Math.PI * radius ** 2
+        console.log(area.toFixed(2));
+    } else {
+        console.log(`We can not calculate the circle area, because we receive a ${typeof(radius)}.`)
+    }
+}
+
+findArea(5);
