1b2a8ae4e76b306b61c4f8eec3a4e1bb11f11398
Mitko Stoyanov <mitko_st6@abv.bg>
2022-05-22 23:45:50 +0300

    Homework.

---
+++ JavaScript-Advanced/Syntax, Functions and Statements/day_of_week.js
@@ -0,0 +1,19 @@
+function findDay(day) {
+    let result = 0;
+
+    switch(day) {
+        case 'Monday': result = 1; break;
+        case 'Tuesday': result = 2; break;
+        case 'Wednesday': result = 3; break;
+        case 'Thursday': result = 4; break;
+        case 'Friday': result = 5; break;
+        case 'Saturday': result = 6; break;
+        case 'Sunday': result = 7; break;
+        default: result = 'error'
+    }
+
+    console.log(result);
+}
+
+findDay('Friday');
+findDay('Invalid');
