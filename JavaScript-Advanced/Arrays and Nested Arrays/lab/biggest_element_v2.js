6118f32a7691fc430032d6d8181b6287bdec2c73
Mitko Stoyanov <mitko_st6@abv.bg>
2022-05-28 19:49:51 +0300

    Classwork.

---
+++ JavaScript-Advanced/Arrays and Nested Arrays/lab/biggest_element_v2.js
@@ -0,0 +1,6 @@
+function findBiggest(matrix) {
+    let biggestNum = Number.MIN_SAFE_INTEGER;
+    console.log(biggestNum = Math.max(...matrix.flat()));
+}
+
+findBiggest([[3, 5, 7, 12],[-1, 4, 33, 2],[8, 3, 0, 4]] );
