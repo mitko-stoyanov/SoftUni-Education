3b8c9153791c85a091d6e90b58ce25a74239682b
Mitko Stoyanov <mitko_st6@abv.bg>
2022-06-02 23:07:47 +0300

    Homework.

---
+++ JavaScript-Advanced/Arrays and Nested Arrays/lab/negative_positive.js
@@ -0,0 +1,18 @@
+function solve(array){
+    let result = [];
+
+    array.forEach(element => {
+        if(element < 0){
+            result.unshift(element);
+        } else{
+            result.push(element);
+        }
+    });
+
+    result.forEach(element => {
+        console.log(element);
+    })
+}
+
+solve([7, -2, 8, 9]);
+// solve([3, -2, 0, -1]);
