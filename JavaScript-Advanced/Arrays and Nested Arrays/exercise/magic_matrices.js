eec2f0494ef77fa309a009179b3ba5d6e9fa417f
Mitko Stoyanov <mitko_st6@abv.bg>
2022-06-08 23:53:53 +0300

    Homework.

---
+++ JavaScript-Advanced/Arrays and Nested Arrays/exercise/magic_matrices.js
@@ -0,0 +1,25 @@
+function isMatrixMagic(matrix){
+    let isMagic = true;
+
+    for(let i=0; i<matrix.length - 1; i++){
+        let sumRowOne = matrix[i].reduce((a, b) => a + b, 0);
+        let sumRowTwo = matrix[i + 1].reduce((a, b) => a + b, 0);
+        let sumColOne = 0;
+        let sumColTwo = 0;
+
+        for(let j=0; j<matrix.length; j++){
+            sumColOne += matrix[i][[j]];
+            sumColTwo += matrix[i + 1][j];
+            
+        }
+
+        if(sumRowOne != sumRowTwo || sumColOne != sumColTwo){
+            isMagic = false;
+        }
+    }
+
+    return isMagic;
+}
+
+isMatrixMagic([[4, 5, 6],[6, 5, 4],[5, 5, 5]]);
+isMatrixMagic([[11, 32, 45],[21, 0, 1],[21, 1, 1]]);
