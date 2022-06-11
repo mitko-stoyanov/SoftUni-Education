68a0ed4c2fb6f0f9498a57e43acb63af270e52a0
Mitko Stoyanov <mitko_st6@abv.bg>
2022-06-11 23:30:10 +0300

    Homework.

---
+++ JavaScript-Advanced/Objects and Composition/lab/city_taxes.js
@@ -0,0 +1,28 @@
+function cityRec(name, population, treasury) {
+    const result = {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += Math.floor(this.population * this.taxRate);
+        },
+        applyGrowth(percentage) {
+            this.population += Math.floor(this.population * (percentage / 100));
+        },
+        applyRecession(percentage) {
+            this.treasury -= Math.floor(this.treasury * (percentage / 100));
+        },
+    };
+
+    return result;
+}
+
+const city =
+    cityRec('Tortuga',
+        7000,
+        15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
