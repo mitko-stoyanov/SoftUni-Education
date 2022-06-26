785404410515eac91ef5c9fa953a35bb3d55ef23
Mitko Stoyanov <mitko_st6@abv.bg>
2022-06-26 21:37:00 +0300

    Homework.

---
+++ JavaScript-Advanced/DOM Introduction/lab/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const linkBtn = document.getElementById('more');
+    const text = document.getElementById('text');
+    linkBtn.style.display = 'none';
+    text.style.display = 'inline';
+}
