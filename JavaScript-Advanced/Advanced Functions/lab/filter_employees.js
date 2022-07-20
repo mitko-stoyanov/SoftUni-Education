e807f434a0c8bd619c87f4f57d1dbd554a27447e
Mitko Stoyanov <mitko_st6@abv.bg>
2022-07-20 22:34:41 +0300

    Homework.

---
+++ JavaScript-Advanced/Advanced Functions/lab/filter_employees.js
@@ -0,0 +1,30 @@
+function solve(data, criteria) {
+    const arr = JSON.parse(data);
+    const [key, value] = criteria.split('-');
+    const filtered = arr.filter(x => x[key] === value);
+   
+    let counter = 0;
+    filtered.forEach(x => console.log(`${counter++}. ${x.first_name} ${x.last_name} - ${x.email}`))
+}
+
+solve(`[{
+    "id": "1",
+    "first_name": "Ardine",
+    "last_name": "Bassam",
+    "email": "abassam0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Jost",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  },  
+{
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }]`,
+    'gender-Female');
