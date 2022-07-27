647cb4c3268dee6992ab3713d248815ffb128169
Mitko Stoyanov <mitko_st6@abv.bg>
2022-07-27 16:19:49 +0300

    Change redirect link in login.js. Added register functionality.

--- JavaScript-Advanced/Exam Preparation/LocalOrphanages/src/app.js
+++ JavaScript-Advanced/Exam Preparation/LocalOrphanages/src/app.js
@@ -3,7 +3,9 @@ import { logout } from './api/userService.js';
 
 import { addRender } from './middlewares/render.js';
 import { addSession } from './middlewares/session.js';
+import { createPage } from './views/create.js';
 import { dashboardPage } from './views/dashboard.js';
+import { detailsPage } from './views/details.js';
 import { loginPage } from './views/login.js';
 import { registerPage } from './views/register.js';
 
@@ -15,7 +17,8 @@ page('/register', registerPage)
 page('/login', loginPage);
 page('/logout', onLogout);
 page('/dashboard', dashboardPage);
-
+page('/create', createPage);
+page('/details/:id', detailsPage)
 
 page.start();
 
---
+++ JavaScript-Advanced/Exam Preparation/LocalOrphanages/src/views/register.js
@@ -0,0 +1,46 @@
+import { html } from '../../node_modules/lit-html/lit-html.js'
+import { createSubmitHandler } from '../utils.js';
+import * as userService from '../api/userService.js'
+
+const registerTemplate = (onSubmit) => html`
+<section id="register-page" class="auth">
+    <form @submit=${onSubmit} id="register">
+        <h1 class="title">Register</h1>
+
+        <article class="input-group">
+            <label for="register-email">Email: </label>
+            <input type="email" id="register-email" name="email">
+        </article>
+
+        <article class="input-group">
+            <label for="register-password">Password: </label>
+            <input type="password" id="register-password" name="password">
+        </article>
+
+        <article class="input-group">
+            <label for="repeat-password">Repeat Password: </label>
+            <input type="password" id="repeat-password" name="repeatPassword">
+        </article>
+
+        <input type="submit" class="btn submit-btn" value="Register">
+    </form>
+</section>
+`;
+
+export function registerPage(ctx) {
+    ctx.render(registerTemplate(createSubmitHandler(ctx, onSubmit)));
+}
+
+async function onSubmit(ctx, data, event) {
+    if (data.email == '' || data.password == '') {
+        return alert('All fields are required!');
+    }
+
+    if (data.password != data['repeatPassword']) {
+        return alert('Passwords don\'t match')
+    }
+
+    await userService.register(data.email, data.password);
+    event.target.reset();
+    ctx.page.redirect('/dashboard')
+}
