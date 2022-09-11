bc65c43deb85c71a8b3102ccb5ee840adbd797a9
Mitko Stoyanov <mitko_st6@abv.bg>
2022-09-11 23:50:54 +0300

    Added edit.js

---
+++ JavaScript-Advanced/Exam Preparation/OnlineBooksLibrary/src/views/edit.js
@@ -0,0 +1,70 @@
+import { html } from '../../node_modules/lit-html/lit-html.js'
+import * as booksService from '../api/booksService.js'
+import { createSubmitHandler } from '../util.js';
+
+const editTemplate = (book, onSubmit) => html`
+<section id="edit-page" class="edit">
+    <form @submit=${onSubmit} id="edit-form" action="#" method="">
+        <fieldset>
+            <legend>Edit my Book</legend>
+            <p class="field">
+                <label for="title">Title</label>
+                <span class="input">
+                    <input type="text" name="title" id="title" value="${book.title}">
+                </span>
+            </p>
+            <p class="field">
+                <label for="description">Description</label>
+                <span class="input">
+                    <textarea name="description"
+                        id="description">${book.description}</textarea>
+                </span>
+            </p>
+            <p class="field">
+                <label for="image">Image</label>
+                <span class="input">
+                    <input type="text" name="imageUrl" id="image" value="${book.imageUrl}">
+                </span>
+            </p>
+            <p class="field">
+                <label for="type">Type</label>
+                <span class="input">
+                    <select id="type" name="type" value="Fiction">
+                        <option value="Fiction">Fiction</option>
+                        <option value="Romance">Romance</option>
+                        <option value="Mistery">Mistery</option>
+                        <option value="Classic">Clasic</option>
+                        <option value="Other">Other</option>
+                    </select>
+                </span>
+            </p>
+            <input class="button submit" type="submit" value="Save">
+        </fieldset>
+    </form>
+</section>
+`;
+// Ако имаме textarea всички values трябва да станат .value
+export async function editPage(ctx) {
+    const bookId = ctx.params.id;
+    const book = await booksService.getById(bookId);
+
+    ctx.render(editTemplate(book, createSubmitHandler(ctx, onSubmit)));
+}
+
+async function onSubmit(ctx, data, event) {
+    const bookId = ctx.params.id;
+
+    if (Object.values(data).some(f => f == '')) {
+        return alert('All fields are required!')
+    }
+
+    await booksService.update(bookId, {
+        title: data.title,
+        description: data.description,
+        imageUrl: data.imageUrl,
+        type: data.type
+    });
+
+    event.target.reset();
+    ctx.page.redirect('/details/' + bookId)
+}
